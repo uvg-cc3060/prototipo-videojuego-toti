e8729d0f0fb0b563c0a4f0c515cb9fb2a7273fdc
sam17332 <32148906+sam17332@users.noreply.github.com>
2019-05-13 14:50:21 -0600

    Se agrega movimiento al jugador y la camara

---
+++ Assets/Scripts/camara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara : MonoBehaviour
+{
+	public GameObject jugador;
+	private Vector3 distancia;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		distancia = transform.position - jugador.transform.position;
+	}	
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = jugador.transform.position + distancia;
+	}
+}
---
+++ Assets/Scripts/movimientoJugador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoJugador : MonoBehaviour
+{
+	public float velocidad = 10; 
+	private Rigidbody miRigidbody;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		miRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float vertical = Input.GetAxis("Vertical");
+		float horizonal = Input.GetAxis("Horizontal");
+		miRigidbody.AddForce(new Vector3(horizonal, 0, vertical)*velocidad);
+	}
+}
